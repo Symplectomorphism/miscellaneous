3eaea36809a6631e6106cb86d4137a0c72a15379
Aykut C. Satici <acsatici@gmail.com>
2021-07-22 10:58:31 -0600

    optimizasyon probleminin kurulumu ve aciklamasi

--- dolambac/Julia/question_174.jl
+++ dolambac/Julia/question_174.jl
@@ -14,26 +14,35 @@ function find_best(best_N::Int=0, best_t::Int=0)
 
         @variables model begin
             t[1:N+1], Int       # The time at step k ∈ [1, N].
-            a[1:N+1], Int       # The age of the guy at step k ∈ [1, N].
+            a[1:N+1], Int       # The age of the lady at step k ∈ [1, N].
             s[1:N+1]            # The amount of resources available at step k ∈ [1, N].
             x[1:N,1:3], Bin     # 3 actions are available at step k ∈ [1, N]: 
                                 # Go forward by 33 years, Wait for 11 years, or
-                                # Go backward by 11 years.
+                                # Go backward by 33 years.
             y[1:N+1], Bin       # Auxiliary binary variable to maximize the maximum element of t.
             T, Int
         end
 
-        @constraint(model, t[1]==0)                             # time starts at 0
-        @constraint(model, t[N+1]==0)                           # final time must be back to 0
-        @constraint(model, a[1]==15)                            # initial age
-        @constraint(model, s[1]==21)                            # initial resource amount
+        @constraint(model, t[1]==0)                             # Time starts at 0
+        @constraint(model, t[N+1]==0)                           # Final time must be back to 0
+        @constraint(model, a[1]==15)                            # Initial age
+        @constraint(model, s[1]==21)                            # Initial resource amount
 
-        @constraint(model, [i=1:N], s[i] >= 1)                  # he must have at least one element left to travel
-        @constraint(model, [i=1:N], sum(x[i,:])==1)             # only one decision is allowed at each step
+        @constraint(model, [i=1:N], s[i] >= 1)                  # She must have at least one resource left to travel
+        @constraint(model, [i=1:N], sum(x[i,:])==1)             # Only one decision is allowed at each step
 
-        @constraint(model, [k=1:N-1], x[k,2] + x[k+1,2] >= 1)   # he must wait at least for one step before traveling again.
-        @constraint(model, sum(y) == 1)                         # if y[k] = 1 then T must be less than t[k]
-        @constraint(model, [k=1:N+1], T - t[k] <= X*(1-y[k]))   # We maximize T so that the largest of t[k] increases.
+        @constraint(model, [k=1:N-1], x[k,2] + x[k+1,2] >= 1)   # She must wait at least for one step before traveling again.
+
+        @constraint(model, sum(y) == 1)                         # We want to maximize only one element of t[k], y is an
+                                                                # indicator which must have only one element to be 1 and
+                                                                # other elements will be 0.
+        @constraint(model, [k=1:N+1], T - t[k] <= X*(1-y[k]))   # If y[j] = 1 then T must be less than t[j] 
+                                                                # We will maximize T so that the t[j] increases; but 
+                                                                # t[j] is supposed to be the maximum of t by the y 
+                                                                # construction b/c is y is such that t[k] for k =/= j 
+                                                                # was maximum then we cannot maximize T as much as 
+                                                                # possible, so the mixed integer program will switch 
+                                                                # y[k] = 1 to y[j] = 1!
 
         for k = 1:N
             # Depending on the action x[k,?] taken at step k ∈ [1, N], model the value of the next time t.
@@ -54,7 +63,7 @@ function find_best(best_N::Int=0, best_t::Int=0)
             @constraint(model, s[k+1] - s[k] <= -1 + X*(1-x[k,1]-x[k,3])  )
             @constraint(model, s[k+1] - s[k] >= -1 - X*(1-x[k,1]-x[k,3]) )
 
-            # Depending on the action x[k,?] taken at step k ∈ [1, N], compute the age of the guy.
+            # Depending on the action x[k,?] taken at step k ∈ [1, N], compute the age of the lady.
             @constraint(model, a[k+1]  <= a[k] + X*(1-x[k,1]-x[k,3]) )
             @constraint(model, a[k+1]  >= a[k] - X*(1-x[k,1]-x[k,3]) )
 
